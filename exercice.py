76ddd43b847ac3ee993fe5cadad1bae4962090b9
JamZel <jamyzellag@gmail.com>
2020-09-14 22:35:43 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -19,7 +19,8 @@ def square(a: float) -> float:
 
 
 def average(a: float, b: float, c: float) -> float:
-    print('Entrer la valeur de trois nombres')
+    print('Entrer la valeur de trois nombres: ')
+    a = 
     return 0.0
 
 
