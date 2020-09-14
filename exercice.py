a569133fed8bcb1f3c821d6fd292317fa22e100f
JamZel <jamyzellag@gmail.com>
2020-09-14 22:36:39 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -24,8 +24,8 @@ def average(a: float, b: float, c: float) -> float:
     b = input()
     c = input()
 
-    z= (a+ b+ c)
-    return 0.0
+z= (a + b + c)/3
+    return z
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
